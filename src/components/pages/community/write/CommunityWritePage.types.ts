a9f3a2a3527aaab010acc6c48ec7afc07a0db379
Develing1991 <completed0728@gmail.com>
2023-01-29 06:50:47 +0900

    타입 분리

--- src/components/pages/community/write/CommunityWritePage.types.ts
+++ src/components/pages/community/write/CommunityWritePage.types.ts
@@ -1,5 +1,21 @@
+import { SyntheticEvent, MouseEvent, ChangeEvent } from 'react';
+
 export interface FileUrls {
 	name: string;
 	id: string;
 	file: File;
+}
+
+/* Presenter */
+export interface ICommunityWritePagePresenter {
+	subject: string;
+	show: boolean;
+	onClickSelect: (event: MouseEvent<HTMLButtonElement>) => void;
+	onClickSelectOption: (value: string) => (event: MouseEvent<HTMLLIElement>) => void;
+	onSubmitForm: (event: SyntheticEvent<HTMLButtonElement>) => void;
+	hideSelect: () => void;
+	prepareImageFiles: (files: File[]) => void;
+	previewImageUrls: FileUrls[] | undefined;
+	removeImageFiles: (id: string) => () => void;
+	onChangeInput: (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
 }
